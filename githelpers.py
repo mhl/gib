4e85b8416d74271976d8e00a6a538aa713af9551
Mark Longair <mhl@pobox.com>
2012-06-05 08:43:45 +0100

    Split off code for setup, and generic helpers, into new files

---
+++ githelpers.py
@@ -0,0 +1,25 @@
+from general import *
+
+def has_objects_and_refs(path):
+    '''Returns True if <path>/objects and <path>/refs both exist and
+    (after resolving any symlinks) are directories; returns False
+    otherwise.  The existence of this directory structure is a
+    resonable sanity check on <path> being a git repository'''
+    objects_path = os.path.join(path,"objects")
+    refs_path = os.path.join(path,"refs")
+    return exists_and_is_directory(objects_path) and exists_and_is_directory(refs_path)
+
+def probable_non_bare_repository(path):
+    git_directory_path = os.path.join(path,'.git')
+    return has_objects_and_refs(git_directory_path)
+
+def is_in_another_git_repository(relative_path):
+    if not exists_and_is_directory(relative_path):
+        raise Exception("{} was not a directory".format(relative_path))
+    while True:
+        parent = os.path.dirname(relative_path)
+        if not parent:
+            return False
+        if probable_non_bare_repository(parent):
+            return True
+        relative_path = parent
---
+++ gitsetup.py
@@ -0,0 +1,239 @@
+import os
+import sys
+from subprocess import call, STDOUT
+
+from optparse import OptionParser
+from configparser import RawConfigParser
+
+from general import *
+from errors import Errors
+from githelpers import *
+
+class OptionFrom:
+    '''enum-like values to indicate the source of different options, used in
+    directory_to_backup_from, git_directory_from and branch_from'''
+    COMMAND_LINE = 1
+    CONFIGURATION_FILE = 2
+    DEFAULT_VALUE = 3
+    string_versions = { COMMAND_LINE : "command line",
+                        CONFIGURATION_FILE : "configuration file",
+                        DEFAULT_VALUE : "default value" }
+
+class GibSetup:
+    def __init__(self, command_line_options):
+
+        self.configuration_file = '.gib.conf'
+
+        self.directory_to_backup = None
+        self.directory_to_backup_from = None
+
+        self.git_directory = None
+        self.git_directory_from = None
+
+        self.branch = None
+        self.branch_from = None
+
+        if command_line_options.directory:
+            self.directory_to_backup = command_line_options.directory
+            self.directory_to_backup_from = OptionFrom.COMMAND_LINE
+        else:
+            if 'HOME' not in os.environ:
+                # Then we can't use HOME as default directory:
+                print("The HOME environment variable was not set",file=sys.stderr)
+                sys.exit(Errors.STRANGE_ENVIRONMENT)
+            self.directory_to_backup = os.environ['HOME']
+            self.directory_to_backup_from = OptionFrom.DEFAULT_VALUE
+
+        # We need to make sure that this is an absolute path before
+        # changing directory:
+
+        self.directory_to_backup = os.path.abspath(self.directory_to_backup)
+
+        if not exists_and_is_directory(self.directory_to_backup):
+            sys.exit(Errors.DIRECTORY_TO_BACKUP_MISSING)
+
+        # Now we know the directory that we're backing up, try to load the
+        # config file:
+
+        configuration = RawConfigParser()
+        configuration.read(os.path.join(self.directory_to_backup,
+                                        self.configuration_file))
+
+        # Now set the git directory:
+
+        if command_line_options.git_directory:
+            self.git_directory = command_line_options.git_directory
+            self.git_directory_from = OptionFrom.COMMAND_LINE
+        elif configuration.has_option('repository','git_directory'):
+            self.git_directory = configuration.get('repository','git_directory')
+            self.git_directory_from = OptionFrom.CONFIGURATION_FILE
+        else:
+            self.git_directory = os.path.join(directory_to_backup,'.git')
+            self.git_directory_from = OptionFrom.DEFAULT_VALUE
+
+        if not os.path.isabs(self.git_directory):
+            print("The git directory must be an absolute path.",file=sys.stderr)
+            sys.exit(Errors.GIT_DIRECTORY_RELATIVE)
+
+        # And finally the branch:
+
+        if command_line_options.branch:
+            self.branch = command_line_options.branch
+            self.branch_from = OptionFrom.COMMAND_LINE
+        elif configuration.has_option('repository','branch'):
+            self.branch = configuration.get('repository','branch')
+            self.branch_from = OptionFrom.CONFIGURATION_FILE
+        else:
+            self.branch = 'master'
+            self.branch_from = OptionFrom.DEFAULT_VALUE
+
+        # Check that the git_directory ends in '.git':
+
+        if not re.search('\.git/*$',self.git_directory):
+            print("The git directory ({}) did not end in '.git'".format(self.git_directory),file=sys.stderr)
+            sys.exit(Errors.BAD_GIT_DIRECTORY)
+
+        # Also check that it actually exists:
+
+        if not os.path.exists(self.git_directory):
+            print("The git directory '{}' does not exist.".format(self.git_directory),file=sys.stderr)
+            sys.exit(Errors.GIT_DIRECTORY_MISSING)
+
+    def get_directory_to_backup(self):
+        return self.directory_to_backup
+
+    def get_git_directory(self):
+        return self.git_directory
+
+    def get_branch(self):
+        return self.branch
+
+    def print_settings(self):
+        print('''Settings for backup:
+backing up the directory {} (set from the {})
+... to the branch "{}" (set from the {})
+... in the git repository {} (set from the {})'''.format(
+                self.directory_to_backup,
+                OptionFrom.string_versions[self.directory_to_backup_from],
+                self.branch,
+                OptionFrom.string_versions[self.branch_from],
+                self.git_directory,
+                OptionFrom.string_versions[self.git_directory_from]),
+              file=sys.stderr)
+
+    def get_invocation(self):
+        '''Return an invocation that would run the script with options
+        that will set directory_to_backup, git_directory and branch as on
+        this invocation.  After init has been called, we can just specify
+        the directory to backup, since the configuration file .gib.conf in
+        that directory will store the git_directory and the branch.  If
+        the directory to backup is just the current user's home directory,
+        then that doesn't need to be specified either.'''
+        invocation = sys.argv[0]
+        if self.directory_to_backup != os.environ['HOME']:
+            invocation += " " + "--directory=" + shellquote(self.directory_to_backup)
+        return invocation
+
+    def git(self,rest_of_command):
+        '''Create an list (suitable for passing to subprocess.call or
+        subprocess.check_call) which runs a git command with the correct
+        git directory and work tree'''
+        return [ "git",
+                 "--git-dir="+self.git_directory,
+                 "--work-tree="+self.directory_to_backup ] + rest_of_command
+
+    def git_for_shell(self):
+        '''Returns a string with shell-safe invocation of git which can be used
+        in calls that are subject to shell interpretation.'''
+        return "git --git-dir="+shellquote(self.git_directory)+" --work-tree="+shellquote(self.directory_to_backup)
+
+    def git_initialized(self):
+        '''Returns True if it seems as if the git directory has already
+        been intialized, and returns False otherwise'''
+        return has_objects_and_refs(self.git_directory)
+
+    def abort_if_not_initialized(self):
+        '''Check that the git repository exists and exit otherwise'''
+        if not self.git_initialized():
+            print("You don't seem to have initialized {} for backup.".format(self.directory_to_backup),file=sys.stderr)
+            print("Please use '{} init' to initialize it".format(self.get_invocation()),file=sys.stderr)
+            sys.exit(Errors.REPOSITORY_NOT_INITIALIZED)
+
+    def check_ref(self,ref):
+        '''Returns True if a ref can be resolved to a commit and False
+        otherwise.'''
+        return 0 == call(self.git(["rev-parse","--verify",ref]),stdout=open('/dev/null','w'),stderr=STDOUT)
+
+    def check_tree(self,tree):
+        '''Returns True if 'tree' can be understood as a tree, e.g. with
+        "git ls-tree" or false otherwise'''
+        with open('/dev/null','w') as null:
+            return 0 == call(self.git(["ls-tree",tree]),stdout=null,stderr=STDOUT)
+
+    def set_HEAD_to(self,ref):
+        '''Update head to point to a particular branch, without touching
+        the index or the working tree'''
+        check_call(self.git(["symbolic-ref","HEAD","refs/heads/{}".format(ref)]))
+
+    def currently_on_correct_branch(self):
+        '''Return True if HEAD currently points to 'self.branch', and
+        return False otherwise.'''
+        p = Popen(self.git(["symbolic-ref","HEAD"]),stdout=PIPE)
+        c = p.communicate()
+        if 0 != p.returncode:
+            print("Finding what HEAD points to failed",file=sys.stderr)
+            sys.exit(Errors.FINDING_HEAD)
+        result = c[0].decode().strip()
+        if self.branch == result:
+            return True
+        elif ("refs/heads/"+self.branch) == result:
+            return True
+        else:
+            return False
+
+    def switch_to_correct_branch(self):
+        self.set_HEAD_to(branch)
+        self.abort_unless_HEAD_exists()
+        # Also reset the index to match HEAD.  Otherwise things go
+        # horribly wrong when switching from backing up one computer to
+        # another, since the index is still that from the first one.
+        print("Now working on a new branch, so resetting the index to match...",file=sys.stderr)
+        check_call(self.git(["read-tree","HEAD"]))
+
+    def config_value(self,key):
+        '''Retrieve the git config value for "key", or return
+        None if it is not defined'''
+        p = Popen(self.git(["config",key]),stdout=PIPE)
+        c = p.communicate()
+        if 0 == p.returncode:
+            # Then check that the option is right:
+            return c[0].decode().strip()
+        else:
+            return None
+
+    def set_config_value(self,key,value):
+        check_call(self.git(["config",key,value]))
+
+    def unset_config_value(self,key):
+        call(self.git(["config","--unset",key]))
+
+    def abort_unless_particular_config(self,key,required_value):
+        '''Unless the git config has "required_value" set for "key", exit.'''
+        current_value = self.config_value(key)
+        if current_value:
+            if current_value != required_value:
+                print("The current value for {} is {}, should be: {}".format(key,current_value,required_value),file=sys.stderr)
+                sys.exit(Errors.GIT_CONFIG_ERROR)
+        else:
+            print("The {} config option was not set, setting to {}".format(key,required_value),file=sys.stderr)
+            self.set_config_value(key,required_value)
+
+    def abort_unless_no_auto_gc(self):
+        '''Exit unless git config has gc.auto set to "0"'''
+        self.abort_unless_particular_config("gc.auto","0")
+
+    def abort_unless_HEAD_exists(self):
+        if not self.check_ref("HEAD"):
+            print("The branch you are trying to back up to does not exist.",file=sys.stderr)
+            print("(Perhaps you haven't run \"{} init\")".format(self.get_invocation()),file=sys.stderr)
+            sys.exit(Errors.NO_SUCH_BRANCH)
