57727976eea6e880760bbd7e15dac4449ec011cd
Mark Longair <mhl@pobox.com>
2013-11-17 11:02:35 +0000

    Revert "Add another version of find_git_repositories"
    This reverts commit 981b911943882367be6787147120fd08e9a4f501.

--- githelpers.py
+++ githelpers.py
@@ -23,30 +23,3 @@ def is_in_another_git_repository(relative_path):
         if probable_non_bare_repository(parent):
             return True
         relative_path = parent
-
-# default parameter was: setup.get_directory_to_backup()
-def find_git_repositories(start_path):
-    '''Use the find-git-repos command to return a list of all directories
-    which are working trees with git repositories.  (In other words, this
-    does not find bare repositories.)
-
-    This used to be done by find-git-repos, but that inexplicably missed
-    some paths.  This version isn't quite good enough, because it doesn't
-    pay attention to .gitignore files - FIXME when I have time.  Instead
-    for the moment if finds all non-bare git repositories, even if they
-    would have been ignored.'''
-    new_start_path = re.sub(r'/*$', '/', start_path)
-    git_directories = []
-    for root, dirs, files in os.walk(start_path):
-        directories_to_prune = []
-        for i, potential_git_directory in enumerate(dirs):
-            full_name = os.path.join(root, potential_git_directory)
-            if probable_non_bare_repository(full_name):
-                relative_name = re.sub('^' + re.escape(new_start_path),
-                                       '',
-                                       full_name)
-                git_directories.append(relative_name)
-                directories_to_prune.append(i)
-        for i in reversed(directories_to_prune):
-            del dirs[i]
-    return git_directories
